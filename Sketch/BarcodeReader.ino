58a70cb0f60d6fefe970b924aa691070febe7940
SaliBasri <91484560+SaliBasri@users.noreply.github.com>
2022-10-17 13:46:34 +0200

    The current files for Arduino
    We can now Read through the Database:
    More work and tests on Servos are required

---
+++ Sketch/BarcodeReader.ino
@@ -0,0 +1,11 @@
+/*
+	BarcodeReader looks for the values received through the camera
+    @return -> sequece of digits / "Not Detected" */
+String BarcodeReader() {
+  String s = "Not detected";
+    
+  /// TODO: Modify function to read from the scanner
+    
+  //Serial.println("Went through BarcodeReader");
+  return s;
+}
---
+++ Sketch/ButtonReader.ino
@@ -0,0 +1,15 @@
+/*
+	BurronReader awaits value change from the buttons
+    @return -> the button pressed / 0 if nothing is pressed 
+    Remark: Possible problems:
+    	if a button gets stuck we are doomed :) 
+        if multiple buttons are pressed at the same time... 
+        	... behavior can't be predicted*/
+int ButtonReader() {
+  int buttonPressed = digitalRead(button1Pin);
+  buttonPressed = max(buttonPressed,digitalRead(button2Pin)*2);
+  buttonPressed = max(buttonPressed,digitalRead(button3Pin)*3);
+  buttonPressed = max(buttonPressed,digitalRead(button4Pin)*4);
+                          
+  return buttonPressed;
+}
